55323642ed76b0815c60db7dd83f7c7512e944ff
ruzny <ruzny@ruzny.com>
2020-03-21 17:47:22 +0530

    local-cases templates & working on redux stuffs of local-cases

--- src/pages/LocalCase.js
+++ src/pages/LocalCase.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+// REDUX
+import { connect } from 'react-redux';
+
+import TopStat from '../components/TopStat';
+import BottomStat from '../components/BottomStat';
+import Spinner from '../layouts/Spinner';
 
 const LocalCase = () => {
+  const useStyles = {
+    countrySelect: {
+      width: '400px',
+      height: '50px',
+      fontSize: '1.4rem',
+      border: 'none',
+      outline: 'none',
+      boxShadow: '1px 1px 1px 1px #111'
+    }
+  };
+
+  const onChange = e => {};
+
   return (
-    <div>
-      <p>Local Cases</p>
+    <div className='LocalCases'>
+      <TopStat />
+      <br />
+      <br />
+      <select
+        onChange={e => onChange(e)}
+        style={useStyles.countrySelect}
+      ></select>
+      <br />
+      <br />
+      <BottomStat />
     </div>
   );
 };
 
-export default LocalCase;
+LocalCase.propTypes = {};
+
+const mapStateToProps = state => ({});
+
+export default connect(mapStateToProps, {})(LocalCase);
